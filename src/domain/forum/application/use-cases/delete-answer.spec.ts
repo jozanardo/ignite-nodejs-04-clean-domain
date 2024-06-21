8e7a7ebdcf66601e06e9799ccb33fc587692d6be
João Augusto <62316215+jozanardo@users.noreply.github.com>
2024-06-20 22:06:29 -0300

    refactor: create Functional Error Handling

--- src/domain/forum/application/use-cases/delete-answer.spec.ts
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -2,6 +2,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
 import { DeleteAnswerUseCase } from './delete-answer'
 import { makeAnswer } from 'test/factories/make-answer'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository
 let sut: DeleteAnswerUseCase
@@ -36,11 +37,12 @@ describe('Delete answer', () => {
 
     await inMemoryAnswersRepository.create(newQuestion)
 
-    await expect(() => {
-      return sut.execute({
-        authorId: 'author-2',
-        answerId: 'answer-1',
-      })
-    }).rejects.toBeInstanceOf(Error)
+    const result = await sut.execute({
+      answerId: 'answer-1',
+      authorId: 'author-2',
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(NotAllowedError)
   })
 })
